b4b1e37ce9e8b6afbea8172964ec65bf91e094c2
Rich Rein <rlrein@fedins.com>
2025-04-21 07:34:21 -0500

    Update .gitignore

--- src/envs/dest.cjs
+++
@@ -1,9 +0,0 @@
-const portConfig = {
-  clientId: "7vlkhNyX6dIHdgiNrH6SQa0ssD9bx8bx",
-  clientSecret:
-    "LPes5HohyJUSUKECgBVA6ldLS7h4sjT8q5aIKXij7UDsuACpoZjnEkPrf4gQIPLn",
-  envName: "dest",
-  portDomain: "https://api.us.port.io/v1",
-};
-
-module.exports = portConfig;
--- src/envs/source.cjs
+++
@@ -1,9 +0,0 @@
-const portConfig = {
-  clientId: "e3AYKre9yvAKHkzpqLZFBbIZZmEenx78",
-  clientSecret:
-    "vWl7H5il1HF1d9G66R75FGSmAlCcGo8mAtt0qbvq69GP2Rem4JZaUSL9xmuGXIgZ",
-  envName: "source",
-  portDomain: "https://api.port.io/v1",
-};
-
-module.exports = portConfig;
