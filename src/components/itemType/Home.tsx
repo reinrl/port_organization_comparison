da88d001675b05c7fcf59383bdb9076f27ab47a5
Rich Rein <rlrein@fedins.com>
2025-10-20 13:24:03 -0500

    Remove web version (CORS prevents calls to their APIs from the web app)

--- src/components/itemType/Home.tsx
+++ src/components/itemType/Home.tsx
@@ -1,14 +1,9 @@
 import Container from "react-bootstrap/Container";
-import CorrectRunUrl from "../actions/CorrectRunUrl";
 
 export default function Home() {
   return (
     <Container>
-      <h1>
-        Select an option from the menu above to view the current diff.
-      </h1>
-      <hr />
-      <CorrectRunUrl />
+      <h1>Select an option from the menu above to view the current diff.</h1>
     </Container>
   );
 }
